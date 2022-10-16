43ea85c9e365e5edaf870af9a87d585f9c88bde6
Connor <kor.connor@gmail.com>
2022-10-16 11:34:33 +0900

    basic auth

--- main/example.go
+++ main/example.go
@@ -2,40 +2,38 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"log"
-	"time"
+	"net/http"
 )
 
-func Logger() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		t := time.Now()
-
-		// Set example variable
-		c.Set("example", "12345")
-
-		// before request
-
-		c.Next()
-
-		// after request
-		latency := time.Since(t)
-		log.Print(latency)
-
-		// access the status we are sending
-		status := c.Writer.Status()
-		log.Println(status)
-	}
+// simulate some private data
+var secrets = gin.H{
+	"foo":    gin.H{"email": "foo@bar.com", "phone": "123433"},
+	"austin": gin.H{"email": "austin@example.com", "phone": "666"},
+	"lena":   gin.H{"email": "lena@guapa.com", "phone": "523443"},
 }
 
 func main() {
-	r := gin.New()
-	r.Use(Logger())
+	r := gin.Default()
 
-	r.GET("/test", func(c *gin.Context) {
-		example := c.MustGet("example").(string)
+	// Group using gin.BasicAuth() middleware
+	// gin.Accounts is a shortcut for map[string]string
+	authorized := r.Group("/admin", gin.BasicAuth(gin.Accounts{
+		"foo":    "bar",
+		"austin": "1234",
+		"lena":   "hello2",
+		"manu":   "4321",
+	}))
 
-		// it would print: "12345"
-		log.Println(example)
+	// /admin/secrets endpoint
+	// hit "localhost:8080/admin/secrets
+	authorized.GET("/secrets", func(c *gin.Context) {
+		// get user, it was set by the BasicAuth middleware
+		user := c.MustGet(gin.AuthUserKey).(string)
+		if secret, ok := secrets[user]; ok {
+			c.JSON(http.StatusOK, gin.H{"user": user, "secret": secret})
+		} else {
+			c.JSON(http.StatusOK, gin.H{"user": user, "secret": "NO SECRET :("})
+		}
 	})
 
 	// Listen and serve on 0.0.0.0:8080

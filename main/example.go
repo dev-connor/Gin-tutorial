37920781e0327713f31308e14dfcc792981d089a
Connor <kor.connor@gmail.com>
2022-10-16 12:42:02 +0900

    테스트파일 작성

--- main/example.go
+++ main/example.go
@@ -1,39 +1,20 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-	"github.com/gin-gonic/gin/binding"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
-type formA struct {
-	Foo string `json:"foo" xml:"foo" binding:"required"`
-}
-
-type formB struct {
-	Bar string `json:"bar" xml:"bar" binding:"required"`
-}
-
-func SomeHandler(c *gin.Context) {
-	objA := formA{}
-	objB := formB{}
-	// This reads c.Request.Body and stores the result into the context.
-	if errA := c.ShouldBindBodyWith(&objA, binding.JSON); errA == nil {
-		c.String(http.StatusOK, `the body should be formA`)
-		// At this time, it reuses body stored in the context.
-	} else if errB := c.ShouldBindBodyWith(&objB, binding.JSON); errB == nil {
-		c.String(http.StatusOK, `the body should be formB JSON`)
-		// And it can accepts other formats
-	} else if errB2 := c.ShouldBindBodyWith(&objB, binding.XML); errB2 == nil {
-		c.String(http.StatusOK, `the body should be formB XML`)
-	} else {
-		//...
-	}
+func setupRouter() *gin.Engine {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(http.StatusOK, "pong")
+	})
+	return r
 }
 
 func main() {
-	r := gin.Default()
-	r.POST("/", SomeHandler)
-
-	r.Run()
+	r := setupRouter()
+	r.Run(":8080")
 }

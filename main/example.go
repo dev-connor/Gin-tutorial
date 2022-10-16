9feed09afcf49524c3823cd27931080e3a129be6
Connor <kor.connor@gmail.com>
2022-10-16 12:12:41 +0900

    request 를 여러 구조체로 받기

--- main/example.go
+++ main/example.go
@@ -2,34 +2,38 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"log"
-	"time"
+	"github.com/gin-gonic/gin/binding"
+	"net/http"
 )
 
-// simulate some private data
+type formA struct {
+	Foo string `json:"foo" xml:"foo" binding:"required"`
+}
+
+type formB struct {
+	Bar string `json:"bar" xml:"bar" binding:"required"`
+}
+
+func SomeHandler(c *gin.Context) {
+	objA := formA{}
+	objB := formB{}
+	// This reads c.Request.Body and stores the result into the context.
+	if errA := c.ShouldBindBodyWith(&objA, binding.JSON); errA == nil {
+		c.String(http.StatusOK, `the body should be formA`)
+		// At this time, it reuses body stored in the context.
+	} else if errB := c.ShouldBindBodyWith(&objB, binding.JSON); errB == nil {
+		c.String(http.StatusOK, `the body should be formB JSON`)
+		// And it can accepts other formats
+	} else if errB2 := c.ShouldBindBodyWith(&objB, binding.XML); errB2 == nil {
+		c.String(http.StatusOK, `the body should be formB XML`)
+	} else {
+		//...
+	}
+}
+
 func main() {
 	r := gin.Default()
+	r.POST("/", SomeHandler)
 
-	r.GET("/long_async", func(c *gin.Context) {
-		// create copy to be used inside the goroutine
-		//cCp := c.Copy()
-		go func() {
-			// simulate a long task with time.Sleep(). 5 seconds
-			time.Sleep(5 * time.Second)
-
-			// note that you are using the copied context "cCp", IMPORTANT
-			log.Println("Done! in path " + c.Request.URL.Path)
-		}()
-	})
-
-	r.GET("/long_sync", func(c *gin.Context) {
-		// simulate a long task with time.Sleep(). 5 seconds
-		time.Sleep(5 * time.Second)
-
-		// since we are NOT using a goroutine, we do not have to copy the context
-		log.Println("Done! in path " + c.Request.URL.Path)
-	})
-
-	// Listen and serve on 0.0.0.0:8080
-	r.Run(":8080")
+	r.Run()
 }

a5bc8af4b7fe7cb806a5caab41109e2dac9058e7
Connor <kor.connor@gmail.com>
2022-10-16 10:55:40 +0900

    ShouldBindUri 메서드

--- main/example.go
+++ main/example.go
@@ -1,39 +1,25 @@
 package main
 
 import (
-	"log"
 	"net/http"
-	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
 type Person struct {
-	Name       string    `form:"name"`
-	Address    string    `form:"address"`
-	Birthday   time.Time `form:"birthday" time_format:"2006-01-02" time_utc:"1"`
-	CreateTime time.Time `form:"createTime" time_format:"unixNano"`
-	UnixTime   time.Time `form:"unixTime" time_format:"unix"`
+	ID   string `uri:"id" binding:"required,uuid"`
+	Name string `uri:"name" binding:"required"`
 }
 
 func main() {
 	route := gin.Default()
-	route.POST("/testing", startPage)
-	route.Run(":8085")
-}
-
-func startPage(c *gin.Context) {
-	var person Person
-	// If `GET`, only `Form` binding engine (`query`) used.
-	// If `POST`, first checks the `content-type` for `JSON` or `XML`, then uses `Form` (`form-data`).
-	// See more at https://github.com/gin-gonic/gin/blob/master/binding/binding.go#L88
-	if c.ShouldBind(&person) == nil {
-		log.Println(person.Name)
-		log.Println(person.Address)
-		log.Println(person.Birthday)
-		log.Println(person.CreateTime)
-		log.Println(person.UnixTime)
-	}
-
-	c.String(http.StatusOK, "Success")
+	route.GET("/:name/:id", func(c *gin.Context) {
+		var person Person
+		if err := c.ShouldBindUri(&person); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"msg": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{"name": person.Name, "uuid": person.ID})
+	})
+	route.Run(":8088")
 }
